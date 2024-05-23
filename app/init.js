8ec0e0196916caa7fb93908bd6a3615a51228855
Lucas <lucas.larnicol@gmail.com>
2024-05-23 16:07:36 +0200

    [init]

---
+++ app/init.js
@@ -0,0 +1,50 @@
+export const params = {
+    tresor: 1000,
+    salaries: {
+        nombre : 10,
+        niveau : 0
+    },
+    capital_constant: {
+        machines : {
+            nombre: 2,
+            niveau: 0
+        }
+    }
+};
+
+export const dataProdutivite = {
+    machines: [
+        0,
+        40,
+        200,
+        800,
+        2000,
+        4000
+    ],
+    salaries: [
+        20,
+        22,
+        24,
+        24,
+        24,
+        25
+    ]
+}
+export const dataCouts = {
+    machines: [
+        10,
+        11,
+        12,
+        15,
+        20,
+        27,
+    ],
+    salaries: [
+        100,
+        110,
+        125,
+        150,
+        200,
+        275
+    ]
+};
