b0f711ebc40206b23b4820fbd29e04fba525fa4a
Lucas <lucas.larnicol@gmail.com>
2024-05-26 12:30:06 +0200

    [fix] anmation

--- app/threeJs/canva.js
+++ app/threeJs/canva.js
@@ -11,8 +11,8 @@ export default function Canva(props) {
   const togglePosition = (fnArrive)=> (state)=> (pos, min, max) => {
     
     if(parseInt(pos) >= min) {state[1](true); return true}
-    if(parseInt(pos) <= max) {state[1](false); fnArrive(); return false}
-    return state[0]
+    if(parseInt(pos) === max) {state[1](false); fnArrive(); return false}
+    if(parseInt(pos) < max) {state[1](false); return false}
   };
 
   const togglePositionEvent = togglePosition(props.venteEvent);
@@ -21,7 +21,8 @@ export default function Canva(props) {
   const params = {
     distance: -600,
     size_distributeur: [20, 20, 20],
-    size_moyen_de_production : [40, 40, 40]
+    size_moyen_de_production : [40, 40, 40],
+    vitesse: 200
   }
 
   return <Canvas orthographic camera={{
@@ -32,14 +33,16 @@ export default function Canva(props) {
     <pointLight position={[-10, 4, -10]} intensity={Math.PI} />
     <Box
       boxGeometrie={params.size_distributeur}
-      position={[0, 0, 0]} 
-      frame={(meshRef)=> (state, delta) => setDirection(meshRef.current.position.x, 0, -600) ? 
-        meshRef.current.position.x -= delta*200 : 
-        meshRef.current.position.x += delta*200
+      position={[0, 0, 0]}
+
+      //quand le navigateur n'est pas ouvert, setDirection ne s'active plus ??
+      frame={(meshRef)=> (state, delta) => setDirection(meshRef.current.position.x, 0, params.distance) ? 
+        meshRef.current.position.x -= delta*params.vitesse : 
+        meshRef.current.position.x += delta*params.vitesse
       }
     />
     <Box 
-      position={[-600, 0, 0]}
+      position={[params.distance, 0, 0]}
       boxGeometrie={params.size_moyen_de_production}
       frame={(meshRef)=> ( state, delta)=> null}
     />
